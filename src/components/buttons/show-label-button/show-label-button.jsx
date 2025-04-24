5c2ab2648c27b475fdab1e05ae1af5537b27ccbf
Arthur Gama <arthur.m.gama99@gmail.com>
2025-04-24 14:00:40 -0300

    ajuste no botão de show-label

--- src/components/buttons/show-label-button/show-label-button.jsx
+++ src/components/buttons/show-label-button/show-label-button.jsx
@@ -1,12 +1,10 @@
 import './show-label-button.css'
 
 const ShowLabelButton = ({label = 'CLick to Show Label'}) => {
-    const showLabel = (label) => {
-        alert(`A label desse botão é "${label.target.innerHTML}"`)
-        console.log(label)
-    }
-
-    return  <button className='showLabelButton' onClick={showLabel}>{label}</button>
+    return  <button 
+                className='showLabelButton' 
+                onClick={(label) => alert(`A label desse botão é "${label.target.innerHTML}"`)}>{label}
+            </button>
 }
 
 export default ShowLabelButton
